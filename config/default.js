1c1f7ee4fc99dcbeb5a14fc2a7dfefbd66eef392
dinusha_dck <dinusha@duosoftware.com>
2017-11-09 18:26:47 +0530

    add postgres connection pool mode config

--- config/default.js
+++ config/default.js
@@ -5,7 +5,8 @@ module.exports = {
     "Password":"DuoS123",
     "Port":5432,
     "Host":"104.236.231.11",
-    "Database":"duo"
+    "Database":"duo",
+    "Cluster": true
   },
 
   //"Redis": {
