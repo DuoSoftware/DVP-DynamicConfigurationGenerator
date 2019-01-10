1ae97f15004eeb600725fe104f2169f8e33b39e7
dinushadck <dinusha@duosoftware.com>
2019-01-10 15:56:06 +0530

    set environment variables

--- XmlExtendedDialplanBuilder.js
+++ XmlExtendedDialplanBuilder.js
@@ -1010,7 +1010,7 @@ var CreateConferenceDialplan = function(reqId, epList, context, destinationPatte
 
 };
 
-var CreateRouteDialerAgentDialplan = function(reqId, context, destinationPattern, transferLegInfo, legTimeout, customerNum, extension, domain, recordingEnabled, tenantId, companyId, skill)
+var CreateRouteDialerAgentDialplan = function(reqId, context, destinationPattern, transferLegInfo, legTimeout, customerNum, extension, domain, recordingEnabled, tenantId, companyId, agentPubKey, campaignId)
 {
     try
     {
@@ -1026,7 +1026,7 @@ var CreateRouteDialerAgentDialplan = function(reqId, context, destinationPattern
 
         var ignoreEarlyM = "ignore_early_media=false";
 
-        var option = util.format('[leg_timeout=%d,origination_caller_id_name=%s,origination_caller_id_number=%s,companyid=%s,tenantid=%s,DVP_CALL_DIRECTION=outbound,ards_skill_display=%s,CALL_LEG_TYPE=AGENT,DVP_ACTION_CAT=DIALER,DVP_OPERATION_CAT=AGENT]', legTimeout, customerNum, customerNum, companyId, tenantId, skill);
+        var option = util.format('[leg_timeout=%d,origination_caller_id_name=%s,origination_caller_id_number=%s,companyid=%s,tenantid=%s,CampaignId=%s,DVP_CALL_DIRECTION=outbound,DIALER_AGENT_EVENT=%s,CALL_LEG_TYPE=AGENT,DVP_ACTION_CAT=DIALER,DVP_OPERATION_CAT=AGENT]', legTimeout, customerNum, customerNum, companyId, tenantId, campaignId, agentPubKey);
 
         var dnis = extension;
 
