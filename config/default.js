06a158405dd1665fe3843e03d05ebf576ceaa695
duoRushaid <64801782+duoRushaid@users.noreply.github.com>
2020-10-02 17:12:58 +0530

    Update default.js
    dynamicPort changed to false

--- config/default.js
+++ config/default.js
@@ -55,7 +55,7 @@ module.exports = {
             "dccaclientHost": "127.0.0.1",
             "dccaclientPort": 4555,
             "dccaclientVersion": "1.0.0.0",
-            "dynamicPort": true
+            "dynamicPort": false
         },
 
     "Token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
